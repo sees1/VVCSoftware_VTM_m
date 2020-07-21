b10837939a925ffcab68dd1f5a8594a95fb5c2b1
fabrice.leleannec <fabrice.leleannec@interdigital.com>
2020-07-21 16:21:29 +0200

    Integration of JVET-R0433

--- source/Lib/CommonLib/Slice.h
+++ source/Lib/CommonLib/Slice.h
@@ -2300,6 +2300,9 @@ public:
   CcAlfFilterParam&      getCcAlfAPSParam()  { return m_ccAlfAPSParam; }
   void                   setHasPrefixNalUnitType( bool b )                                { m_hasPrefixNalUnitType = b;                   }
   bool                   getHasPrefixNalUnitType() const                                  { return m_hasPrefixNalUnitType;                }
+#if JVET_R0433
+  bool chromaPresentFlag;
+#endif
 };
 
 struct WPScalingParam
