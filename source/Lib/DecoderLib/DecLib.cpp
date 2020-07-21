454aed1bf0add638f0b8afc33f439e5fc28b7771
fabrice.leleannec <fabrice.leleannec@interdigital.com>
2020-07-21 16:21:29 +0200

    Integration of JVET-R0433

--- source/Lib/DecoderLib/DecLib.cpp
+++ source/Lib/DecoderLib/DecLib.cpp
@@ -1454,7 +1454,6 @@ void activateAPS(PicHeader* picHeader, Slice* pSlice, ParameterSetManager& param
 
 #if JVET_R0433
       CHECK( sps->getChromaFormatIdc() == CHROMA_400 && lmcsAPS->chromaPresentFlag, "When ChromaArrayType is equal to 0, the value of aps_chroma_present_flag of an LMCS_APS shall be equal to 0");
-
       CHECK( lmcsAPS->getReshaperAPSInfo().maxNbitsNeededDeltaCW - 1 < 0 || lmcsAPS->getReshaperAPSInfo().maxNbitsNeededDeltaCW - 1 > sps->getBitDepth(CHANNEL_TYPE_LUMA) - 2, "The value of lmcs_delta_cw_prec_minus1 of an LMCS_APS shall be in the range of 0 to BitDepth 2, inclusive" );
 #endif
 
