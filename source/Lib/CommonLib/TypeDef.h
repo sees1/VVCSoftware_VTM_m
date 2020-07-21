67cf22747851f153aad8e07bd93a21e8fa201a0c
phil <cowanp@sharplabs.com>
2020-07-21 08:23:51 -0700

    JVET-S0181: Reference picture list information signalling cleanup - signal ltrp_in_slice_header_flag[listIdx][rplsIdx] only when num_ref_entries[listIdx][rplsIdx] is greater than 0 in addition to other conditions.

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -176,6 +176,8 @@
 
 #define JVET_S0185_PROPOSAl1_PICTURE_TIMING_CLEANUP       1 // JVET-S0185: Proposal 1, put syntax element pt_cpb_removal_delay_minus1[] first, followed by similar information for sub-layers, followed by pt_dpb_output_delay
 
+#define JVET_S0182_RPL_SIGNALLING                         1 // JVET-S0182: modifications to rpl information signalling
+
 //########### place macros to be be kept below this line ###############
 #define JVET_S0257_DUMP_360SEI_MESSAGE                    1 // Software support of 360 SEI messages
 
