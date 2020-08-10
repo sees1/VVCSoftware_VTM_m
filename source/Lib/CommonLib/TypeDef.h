fec67d207371d5a84410427202889ed670d42e69
Kammachi Sreedhar <kashyap.kammachi-sreedhar@nokia.com>
2020-08-10 11:57:27 +0300

    PPS extension cleanup v3

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -86,8 +86,6 @@
 
 #define JVET_S0179_CONDITIONAL_SIGNAL_GCI                 1 // JVET-S0179: Conditional signalling of GCI fields
 
-#define JVET_R0064                                        1 // JVET-R0064, aspect 2: Move the flag scaling_matrix_for_lfnst_disabled_flag from the scaling_list_data( ) syntax to the SPS.
-
 #define JVET_S0049_ASPECT4                                1 // JVET-S0049 aspect 4: Constrain the value of pps_alf_info_in_ph_flag to be equal to 0 when the PH is in the SH
 
 #define JVET_S0258_SUBPIC_CONSTRAINTS                     1 // JVET-S0258: sub-picture constraints
@@ -879,16 +877,6 @@ enum SPSExtensionFlagIndex
   SPS_EXT__NEXT           = 3,
   NUM_SPS_EXTENSION_FLAGS = 8
 };
-
-/*
-enum PPSExtensionFlagIndex
-{
-  PPS_EXT__REXT           = 0,
-//PPS_EXT__MVHEVC         = 1, //for use in future versions
-//PPS_EXT__SHVC           = 2, //for use in future versions
-  NUM_PPS_EXTENSION_FLAGS = 8
-};
-*/
 
 // TODO: Existing names used for the different NAL unit types can be altered to better reflect the names in the spec.
 //       However, the names in the spec are not yet stable at this point. Once the names are stable, a cleanup
