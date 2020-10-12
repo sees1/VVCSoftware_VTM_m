36bb013fca4fbaa4dd64c1af9b22fc4a9610a807
delagrangep <philippe.delagrange@interdigital.com>
2020-10-13 01:30:12 +0200

    Fixes, mostly independent from multi-threading. Removed call to PU::spanMotionInfo in CABACReader::prediction_unit() as useless and would harm parallel processing.

--- source/Lib/DecoderLib/DecLib.cpp
+++ source/Lib/DecoderLib/DecLib.cpp
@@ -1225,7 +1225,7 @@ void DecLib::checkSeiInPictureUnit()
   for (uint32_t i = 0; i < seiList.size(); i++)
   {
     uint8_t *payload = std::get<2>(seiList[i]);
-    delete   payload;
+    delete[] payload;
   }
   seiList.clear();
 }
