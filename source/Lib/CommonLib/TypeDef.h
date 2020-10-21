81753497bfbe21c042c2d798eb0bc367c4458597
Karsten Suehring <karsten.suehring@hhi.fraunhofer.de>
2020-10-21 17:08:52 +0200

    remove macro JVET_S0219_ASPECT2_CHANGE_ORDER_APS_PARAMS_TYPE

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -69,7 +69,6 @@
 
  
 
-#define JVET_S0219_ASPECT2_CHANGE_ORDER_APS_PARAMS_TYPE   1 // JVET-S0219 aspect2: change the order to put the aps_params_type before the aps_adaptation_parameter_set_id.
 
 #define JVET_R0270                                        1 // JVET-S0270: Treating picture with mixed RASL and RADL slices as RASL picture
 
