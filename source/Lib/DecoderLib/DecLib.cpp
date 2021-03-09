f86b7e79b2699ef6e68df8e8b78e7daf18ff5817
Shunsuke Iwamura <iwamura.s-gc@nhk.or.jp>
2021-03-10 08:27:18 +0900

    fix for DebugBitstream feature

--- source/Lib/DecoderLib/DecLib.cpp
+++ source/Lib/DecoderLib/DecLib.cpp
@@ -331,6 +331,8 @@ bool tryDecodePicture( Picture* pcEncPic, const int expectedPoc, const std::stri
           }
 
           pcDecLib->updateAssociatedIRAP();
+          pcDecLib->updatePrevGDRInSameLayer();
+          pcDecLib->updatePrevIRAPAndGDRSubpic();
           // LMCS APS will be assigned later in LMCS initialization step
           pcEncPic->cs->picHeader->setLmcsAPS( nullptr );
           if( bitstreamFile )
