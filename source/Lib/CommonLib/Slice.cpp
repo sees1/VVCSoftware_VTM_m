b70b9e0f96d2fa1d84d2b26a1608e0046a841f35
delagrangep <philippe.delagrange@interdigital.com>
2022-01-14 22:10:37 +0100

    Avoid fast skip in case of inter-layer prediction (same POC): return MAX instead of 0

--- source/Lib/CommonLib/Slice.cpp
+++ source/Lib/CommonLib/Slice.cpp
@@ -2306,13 +2306,15 @@ unsigned Slice::getMinPictureDistance() const
     const int currPOC  = getPOC();
     for (int refIdx = 0; refIdx < getNumRefIdx(REF_PIC_LIST_0); refIdx++)
     {
-      minPicDist = std::min( minPicDist, std::abs(currPOC - getRefPic(REF_PIC_LIST_0, refIdx)->getPOC()));
+      if (getRefPic(REF_PIC_LIST_0, refIdx)->layerId == getNalUnitLayerId())
+        minPicDist = std::min( minPicDist, std::abs(currPOC - getRefPic(REF_PIC_LIST_0, refIdx)->getPOC()));
     }
     if( getSliceType() == B_SLICE )
     {
       for (int refIdx = 0; refIdx < getNumRefIdx(REF_PIC_LIST_1); refIdx++)
       {
-        minPicDist = std::min(minPicDist, std::abs(currPOC - getRefPic(REF_PIC_LIST_1, refIdx)->getPOC()));
+        if (getRefPic(REF_PIC_LIST_1, refIdx)->layerId == getNalUnitLayerId())
+          minPicDist = std::min(minPicDist, std::abs(currPOC - getRefPic(REF_PIC_LIST_1, refIdx)->getPOC()));
       }
     }
   }
