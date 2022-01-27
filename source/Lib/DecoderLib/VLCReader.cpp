c5ccf8240cd7d5fc422de1d192ddfe0e9b1b05d1
Jonathan Gan <32784990+horseshoe107@users.noreply.github.com>
2022-01-28 03:34:39 +1100

    Print error message when gci_num_additional_bits is in the range [1..5]
    With msg level ERROR the message is printed to stderr

--- source/Lib/DecoderLib/VLCReader.cpp
+++ source/Lib/DecoderLib/VLCReader.cpp
@@ -4607,6 +4607,14 @@ void HLSyntaxReader::parseConstraintInfo(ConstraintInfo *cinfo, const ProfileTie
       READ_FLAG(symbol, "gci_no_reverse_last_sig_coeff_constraint_flag");         cinfo->setNoReverseLastSigCoeffConstraintFlag(symbol > 0 ? true : false);
       numAdditionalBitsUsed = 6;
     }
+#if JVET_Y0273_GCI_BUGFIX
+    else if (numAdditionalBits > 0)
+    {
+      msg(ERROR, "Warning: the current bitstream is illegal because gci_num_additional_bits was set to the value %d. \
+        gci_num_additional_bits may only be set to 0, 6, or greater than 6.", numAdditionalBits);
+      numAdditionalBitsUsed = 0;
+    }
+#endif
     else
     {
       numAdditionalBitsUsed = 0;
